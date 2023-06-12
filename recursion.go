8cdaa5b82993e4ac21c1d57e09e401816be850bd
Ricardo Lopes <ricardo.lopes@ifood.com.br>
2023-06-11 23:07:07 -0300

    I have a good feeling about this...

---
+++ recursion.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func factorial(number int) int {
+	if number == 0 {
+		return 1
+	}
+	return number * factorial(number-1)
+}
+
+func main() {
+	fmt.Println(factorial(7))
+
+	var fibonacci func(number int) int
+	fibonacci = func(number int) int {
+		if number < 2 {
+			return number
+		}
+		return fibonacci(number-1) + fibonacci(number-2)
+	}
+
+	fmt.Println(fibonacci(7))
+}
